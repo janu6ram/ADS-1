d7320edb4fd2a33706ddeff56c053546ab572498
janu6ram <janu6ram@msitprogram.net>
2019-10-31 18:02:05 +0530

    changes

--- Day1/CombinedArray.java
+++ Day1/CombinedArray.java
@@ -1,20 +1,42 @@
 import java.util.Arrays;
-
+/**
+ * This program is to merge two arrays after
+ * sorting.
+ * condition is the time complexity in worst
+ * case must be N.
+ */
 public class CombinedArray {
     /**
-     * main method
-     * in this program merged two sorted arrays.
-     * but the condition is sort the element before adding
-     * to the combined array.
+     * crr is an empty array in which two
+     * arrays must be merged.
+     */
+    private static int[] crr;
+    /**
+     * main method.
+     * Here two sorted arrays are taken as input
+     * and call the mergesort method.
      * @param args main
      */
     public static void main(String[] args) {
         int[] arr = {1, 3, 4, 17};
         int[] brr = {3, 10, 15, 20, 25};
-        int[] crr = new int[arr.length + brr.length];
+        mergeSort(arr, brr);
+        System.out.println("Our merged array is :");
+        System.out.println(Arrays.toString(crr));
+    }
+    /**
+     * this method is to merge two arrays.
+     * while merging sort element and added to new array crr.
+     * this method has two parameters.
+     * @param arr first sorted array.
+     * @param brr second sorted array.
+     */
+    private static void mergeSort(final int[] arr, final int[] brr) {
+        crr = new int[arr.length + brr.length];
         int index = 0;
         int i = 0;
         int j = 0;
+        // this loop time complexity is N.
         while (i < arr.length && j < brr.length) {
             if (arr[i] < brr[j]) {
                 crr[index++] = arr[i++];
@@ -22,14 +44,15 @@ public class CombinedArray {
                 crr[index++] = brr[j++];
             }
         }
+        // this loop time complexity also N.
+        // to add remaining elements of arr.
         while (i < arr.length) {
             crr[index++] = arr[i++];
         }
-        while (j < arr.length) {
+        // this loop time complexity also N.
+        // to add remaining elements of arr.
+        while (j < brr.length) {
             crr[index++] = brr[j++];
         }
-        System.out.println("Our merged array is :");
-        System.out.println(Arrays.toString(crr));
     }
-
 }
--- Day1/ThreeSum.java
+++ Day1/ThreeSum.java
@@ -5,6 +5,11 @@ import java.util.Arrays;
  */
 public class ThreeSum {
     /**
+     * this variable counts the no of pairs
+     * sum in the given array becomes zero.
+     */
+    private static int count = 0;
+    /**
      * Using for loops we get the sum of two numbers
      * and using Arrays.binarySearch(arr,find) find
      * the sum of two numbers in the array.
@@ -14,21 +19,28 @@ public class ThreeSum {
      */
     public static void main(String[] args) {
         int[] arr = {40, 0, -40, 10, 20, -10, -20, 15, 5, 14};
-        int count = 0;
         Arrays.sort(arr);
-        System.out.println(Arrays.toString(arr));
-        //the time complexity of two for loops is N^2.
-        // and binary search is logN.
-        //then finally it completes with N^2logN.s
-        for(int i = 0; i < arr.length; i++) {
-            for(int j = i + 1; j < arr.length; j++) {
+        threeSum(arr);
+        System.out.println(count);
+    }
+    /**
+     * this method
+     * the time complexity of two for loops is N^2.
+     * and binary search is logN.
+     * then finally it completes with N^2logN.
+     * @param arr int array which is our input.
+     * @return count, shows how many threesum becomes zero.
+     */
+    private static int threeSum(final int[] arr) {
+        for (int i = 0; i < arr.length; i++) {
+            for (int j = i + 1; j < arr.length; j++) {
                 int find = -(arr[i] + arr[j]);
-                int index = Arrays.binarySearch(arr,find);
+                int index = Arrays.binarySearch(arr, find);
                 if (i < j && j < index) {
                     count += 1;
                 }
             }
         }
-        System.out.println(count);
+        return count;
     }
 }
