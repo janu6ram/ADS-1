43460425ace97ce64030df11cccbb27ef428d938
janu6ram <janu6ram@msitprogram.net>
2019-11-05 17:19:33 +0530

    changed mergesort

--- Day4/MergeSort Assignment-3/Solution.java
+++ Day4/MergeSort Assignment-3/Solution.java
@@ -1,36 +1,39 @@
 class Solution{
 	public static String[] mergeSort(String[] arr) {
 		// fill you code Here
-		if(arr.length >= 2) {
-			String[] left = new String[arr.length/2];
-			String[] right = new String[arr.length - arr.length/2];
-			for(int i = 0; i < left.length; i++) {
-				left[i] = arr[i];
-			}
-			for(int i = 0; i < right.length; i++) {
-				right[i] = arr[i + arr.length/2];
-			}
-			mergeSort(left);
-			mergeSort(right);
-			merge(arr, left, right);
-		 }
-		 return arr;
+		String[] aux = new String[arr.length];
+		sort(arr, aux, 0, arr.length - 1);
+		return arr;
 	}
-	public static void merge(String[] arr, String[]left,
-		String[]right) {
-			int a = 0;
-			int b = 0;
-			for (int i = 0; i < arr.length; i++) {
-				if (b >= right.length || (a < left.length
-				&& left[a].compareToIgnoreCase(right[b]) < 0)) {
-					arr[i] = left[a];
-					a++;
-				} else {
-					arr[i] = right[b];
-					b++;
-				}
-			}
-
+	public static void sort(String[] arr, String[] aux, int lo, int hi) {
+		if(hi <= lo) return;
+		int mid = lo + (hi - lo)/2;
+		sort(arr, aux, lo, mid);
+		sort(arr, aux, mid + 1, hi);
+		merge(arr, aux, lo, mid, hi);
 	}
+	public static void merge(String[] arr, String[] aux, int lo, int mid, int hi) {
+		for(int k = lo; k <= hi; k++) {
+			aux[k] = arr[k];
+		}
+		int i = lo;
+		int j = mid + 1;
+		for(int k = lo; k <= hi; k++) {
+			if(i > mid) arr[k] = aux[j++];
+			else if(j > hi) arr[k] = aux[i++];
+			else if(less(aux[j], aux[i])) arr[k] = aux[j++];
+			else arr[k] = aux[i++];
+		}
+	}
+	public static boolean less(String j, String i) {
+		int comp = j.compareTo(i);
+		if(comp < 0) return true;
+		return false;
+	}
+	// public static void main( String[] args) {
+	// 	String[] arr1 = {"the", "of", "and", "to", "a", "in", "for", "is", "on", "that"};
+	// 	String[] arr2 = mergeSort(arr1);
+	// 	System.out.println(Arrays.toString(arr2));
 
+	// }
 }
